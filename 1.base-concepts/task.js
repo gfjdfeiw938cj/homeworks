9a63b993888d35b683847a751b3b89eb928d3a4d
gfjdfeiw938cj <artyrgame@mail.ru>
2021-12-11 20:32:00 +0300

    Мои изменения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,39 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+    
+  let x;
+  let x2;
+  let Discriminant = b**2 - 4*a*c;
+  
+  if(Discriminant > 0){
+    x = (-b + Math.sqrt(Discriminant))/(2*a);
+    x2 = (-b - Math.sqrt(Discriminant))/(2*a);
+    arr.push(x,x2);
+    return arr;
+  } else if(Discriminant === 0){
+    x = -b/2*a;
+    arr.push(x);
+    return arr;
+   } else if(Discriminant < 0)
+    return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  
+  let monthDate = date - new Date();
+
+  let timeСonversion = Math.round(monthDate / (1000 * 60 * 60 * 24 * date.getMonth() * 12)) ; 
+
   let totalAmount;
+  let S = amount - contribution;
+  let P = percent*1/12/100;
+  let monthlyPayment;
 
-  // код для задачи №2 писать здесь
+  monthlyPayment = S * (P + (P / (((1 + P)**timeСonversion) - 1)));
 
-  return totalAmount;
+  totalAmount = monthlyPayment * timeСonversion;
+
+  return Math.round(totalAmount * 100)/100;
+ 
 }
+
